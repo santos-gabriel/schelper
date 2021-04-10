49dab89b4d7f9d12737bf34f693bfe779726ad73
gabriel <gabrielalmeidads@gmail.com>
2021-04-10 20:31:02 -0300

    Adicionando componente de cronograma horizontal e arquivo de estilzação para o componente

---
+++ src/components/HorizontalSchedule.tsx
@@ -0,0 +1,10 @@
+import styles from '../styles/components/HorizontalSchedule.module.css';
+
+
+export function HorizontalSchedule() {
+    return (
+        <>
+            <h1>You are in HorizontalSchedule</h1>
+        </>
+    );
+}
