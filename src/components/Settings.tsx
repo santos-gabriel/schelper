bda062a261cf6627c4c7dc3a66fc14847b3fe9e6
gabriel <gabrielalmeidads@gmail.com>
2021-04-10 20:36:42 -0300

    Adicionando componente de definições e arquivo de estilização do componente

---
+++ src/components/Settings.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Settings.module.css';
+
+export function Settings() {
+    return (
+        <>
+            <h1>You are in Settings</h1>
+        </>
+    );
+}
