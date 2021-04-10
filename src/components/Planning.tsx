74db758e65fa8ea7281242faeaee6959198220fe
gabriel <gabrielalmeidads@gmail.com>
2021-04-10 20:34:56 -0300

    Adicionando componente de planejamento e arquivo de estilização do componente

---
+++ src/components/Planning.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Planning.module.css';
+
+export function Planning() {
+    return (
+        <>
+            <h1>You are in Planning</h1>
+        </>
+    );
+}
