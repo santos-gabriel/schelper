19cff78184f43e11b1b55417def928bf83bbfe5f
gabriel <gabrielalmeidads@gmail.com>
2021-04-10 20:27:57 -0300

    Adicionando mensagem em componente vertical schedule

--- src/components/VerticalSchedule.tsx
+++ src/components/VerticalSchedule.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/components/VerticalSchedule.module.css';
 export function VerticalSchedule() {
     return (
         <>
+            <h1>You are in VerticalSchedule</h1>
         </>
     );
 }
