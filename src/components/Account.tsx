33b77be77bbf7136b161ef84b2cad600cafa39f9
gabriel <gabrielalmeidads@gmail.com>
2021-04-10 20:39:25 -0300

    Adicionando componente para conta e arquivo de estilização do componente

---
+++ src/components/Account.tsx
@@ -0,0 +1,9 @@
+import styles from '../styles/components/Account.module.css';
+
+export function Account() {
+    return (
+        <>
+            <h1>You are in Account</h1>
+        </>
+    );
+}
