c8c6012fc5a2855c91366c048e15ff2a76ee6e52
gabriel <gabrielalmeidads@gmail.com>
2021-04-13 00:42:20 -0300

    Adicionando functions para adicionar valor e remover cookies

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -8,7 +8,30 @@ interface setCookieAndRedirectInterface{
     routerRedirect: String;
 }
 
+interface setCookie {
+    cookieName: String;
+    cookieValue: String;
+}
+
+interface removeCookieAndRedirect {
+    cookieName: String;
+    routerRedirect: String;
+}
+
 export function setCookieAndRedirect (props: setCookieAndRedirectInterface) {     
     cookie.set(props.cookieName.valueOf(), props.cookieValue);      
+    Router.push(encodeURI(props.routerRedirect.valueOf()));
+}
+
+export function setCookie(props: setCookie) {
+    cookie.set(props.cookieName.valueOf(), props.cookieValue)
+}
+
+export function removeCookie(props: string) {
+    cookie.remove(props);
+}
+
+export function removeCookieAndRedirect(props: removeCookieAndRedirect) {
+    cookie.remove(props.cookieName.valueOf());
     Router.push(encodeURI(props.routerRedirect.valueOf()));
 }
