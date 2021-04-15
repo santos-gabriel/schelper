4e3423648be62bff7735632e78db426da75ea16d
Gabriel <gabrielalmeidads@gmail.com>
2021-04-15 14:00:23 -0300

    Adicionando estruturação e estilização da barra vertical de horas

--- src/components/PortraitScheduleTime.tsx
+++ src/components/PortraitScheduleTime.tsx
@@ -1,9 +1,16 @@
 
 
 export function PortraitScheduleTime() {
-    return (
-        <>
-            <h1>You are in Portrait Schedule Time</h1>
-        </>
+    return (        
+        <div>
+            <span>08</span>
+            <span>09</span>
+            <span>10</span>
+            <span>11</span>
+            <span>12</span>
+            <span>13</span>
+            <span>14</span>
+            <span>15</span>
+        </div>        
     );
 }
